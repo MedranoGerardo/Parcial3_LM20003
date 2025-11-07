25ebbeff869d98ab8fc7e5d7136ae26f3e0aa78e
MedranoGerardo <100704058+MedranoGerardo@users.noreply.github.com>
2025-11-07 11:58:38 -0600

    Add files via upload

---
+++ resources/views/tabla_armotizadora.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <input type="">
+
+    <table>
+        <th>Mes</th>
+        <th>Cuota</th>
+        <th>Interes</th>
+        <th>capital</th>
+        <th>saldo pendiente</th>
+    </table>
+</body>
+</html>
---
+++ storage/framework/views/feaec456bcd7283619bb1ead47d07d92.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <input type="">
+
+    <table>
+        <th>Mes</th>
+        <th>Cuota</th>
+        <th>Interes</th>
+        <th>capital</th>
+        <th>saldo pendiente</th>
+    </table>
+</body>
+</html><?php /**PATH /var/www/html/Parcial3_LM20003/resources/views/tabla_armotizadora.blade.php ENDPATH**/ ?>
